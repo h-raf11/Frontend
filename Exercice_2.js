4bc6c982b09d4d5c031839d484f6b0d664c7e9a2
h-raf11 <achrafelbiyani.297@gmail.com>
2025-12-02 09:50:34 +0100

    Exercice 2

---
+++ Exercice_2.js
@@ -0,0 +1,12 @@
+const personnes = [
+  { nom: 'Rami', age: 33, estMembre: true },
+  { nom: 'Fatihi', age: 24, estMembre: false },
+  { nom: 'Chakib', age: 45, estMembre: true },
+  { nom: 'Mounir', age: 37, estMembre: false }
+];
+
+const nomAges = personnes
+  .filter(personne => personne.estMembre === true)
+  .map(personne => personne.nom);
+
+console.log(nomAges);
