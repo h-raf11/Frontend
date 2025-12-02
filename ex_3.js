b39437cd691fcffccd70732143a69c7dff4ee61b
h-raf11 <achrafelbiyani.297@gmail.com>
2025-12-02 09:56:04 +0100

    Exercice 3

---
+++ ex_3.js
@@ -0,0 +1,17 @@
+const inscriptions = [
+  { id: 10, nom: 'Rami', filiere: 'DEV' },
+  { id: 11, nom: 'Kamali', filiere: 'DEV' },
+  { id: 12, nom: 'Fahmi', filiere: 'DEV' },
+  { id: 13, nom: 'Chaouki', filiere: 'DEV' }
+];
+
+function ajouterInscription(liste, newInscription) {
+  return [...liste, newInscription];
+}
+
+
+const inscription = { id: 14, nom: 'Mouad', filiere: 'DEV' };
+
+const newInscriptions = ajouterInscription(inscriptions, inscription);
+
+console.log(newInscriptions);
